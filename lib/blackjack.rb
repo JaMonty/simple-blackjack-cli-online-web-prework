069af11ec9fc5eff8316da1a6aeaebd555b8723d
Jason  Montes <jasone.montes@gmail.com>
2019-03-27 06:35:38 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -15,11 +15,13 @@ def deal_card do
 end
 
 def display_card_total do 
-  
+  it "accepts one argument, the card total" do
+    expect { display_card_total(7) }.to_not raise_error
   # code #display_card_total here
 end
 
-def prompt_user
+def prompt_user do 
+  
   # code #prompt_user here
 end
 
